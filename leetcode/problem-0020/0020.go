f08588a9da5a128a29ecc565b1c482fc8b4487b1
Sean Cheng <blackhorseya@gmail.com>
2021-02-01 10:17:17 +0800

    feat: added problem 0020

---
+++ leetcode/problem-0020/0020.go
@@ -0,0 +1,52 @@
+package problem_0020
+
+func isValid(s string) bool {
+	var tmp []string
+
+	for _, c := range s {
+		switch c {
+		case '(':
+			tmp = append(tmp, string(c))
+			break
+		case '{':
+			tmp = append(tmp, string(c))
+			break
+		case '[':
+			tmp = append(tmp, string(c))
+			break
+		case ')':
+			if len(tmp) == 0 {
+				return false
+			}
+			if end := tmp[len(tmp)-1]; end != "(" {
+				return false
+			}
+			tmp = append(tmp[:len(tmp)-1], tmp[len(tmp)-1+1:]...)
+			break
+		case ']':
+			if len(tmp) == 0 {
+				return false
+			}
+			if end := tmp[len(tmp)-1]; end != "[" {
+				return false
+			}
+			tmp = append(tmp[:len(tmp)-1], tmp[len(tmp)-1+1:]...)
+			break
+		case '}':
+			if len(tmp) == 0 {
+				return false
+			}
+			if end := tmp[len(tmp)-1]; end != "{" {
+				return false
+			}
+			tmp = append(tmp[:len(tmp)-1], tmp[len(tmp)-1+1:]...)
+			break
+		}
+	}
+
+	if len(tmp) != 0 {
+		return false
+	}
+
+	return true
+}
