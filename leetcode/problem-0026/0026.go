69feec5714938375f9edb0a85dced5d987fe5c6c
Sean Cheng <blackhorseya@gmail.com>
2021-02-05 12:34:12 +0800

    refactor: problem 0026

--- leetcode/problem-0026/0026.go
+++ leetcode/problem-0026/0026.go
@@ -1,17 +1,13 @@
 package problem_0026
 
 func removeDuplicates(nums []int) int {
-	if len(nums) <= 1 {
-		return 1
-	}
-
-	ret := 0
-	for i := 0; i< len(nums) - 1; i++ {
-		if nums[i] != nums[i+1] {
+	ret := 1
+	for i := 1; i < len(nums); i++ {
+		if nums[ret-1] != nums[i] {
+			nums[ret] = nums[i]
 			ret++
-			nums[ret] = nums[i+1]
 		}
 	}
 
-	return ret+1
+	return ret
 }
