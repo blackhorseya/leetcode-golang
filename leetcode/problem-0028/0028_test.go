ff05e0bb9d67e2e616de60626f95f65edb58b3ee
Sean Cheng <blackhorseya@gmail.com>
2021-02-08 09:48:53 +0800

    feat: added problem 0028

---
+++ leetcode/problem-0028/0028_test.go
@@ -0,0 +1,38 @@
+package problem_0028
+
+import "testing"
+
+func Test_strStr(t *testing.T) {
+	type args struct {
+		haystack string
+		needle   string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "\"\" \"\" then 0",
+			args: args{haystack: "", needle: ""},
+			want: 0,
+		},
+		{
+			name: "aaaaa bba then -1",
+			args: args{haystack: "aaaaa", needle: "bba"},
+			want: -1,
+		},
+		{
+			name: "hello ll then 2",
+			args: args{haystack: "hello", needle: "ll"},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := strStr(tt.args.haystack, tt.args.needle); got != tt.want {
+				t.Errorf("strStr() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
