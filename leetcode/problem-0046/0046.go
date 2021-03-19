a7e5b1db39752ef66b9d3a95b01b4a241bfcf40c
Sean Cheng <blackhorseya@gmail.com>
2021-03-19 14:10:33 +0800

    feat: added problem 0046

---
+++ leetcode/problem-0046/0046.go
@@ -0,0 +1,23 @@
+package problem_0046
+
+func permute(nums []int) [][]int {
+	var ret [][]int
+
+	dfs([]int{}, nums, &ret)
+
+	return ret
+}
+
+func dfs(currComb, left []int, res *[][]int) {
+	if 0 == len(left) {
+		*res = append(*res, currComb)
+		return
+	}
+	for idx, l := range left {
+		dfs(
+			append(currComb, l),
+			append(append([]int{}, left[:idx]...), left[idx+1:]...),
+			res,
+		)
+	}
+}
---
+++ leetcode/problem-0046/0046_test.go
@@ -0,0 +1,40 @@
+package problem_0046
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_permute(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "[1] then result",
+			args: args{[]int{1}},
+			want: [][]int{{1}},
+		},
+		{
+			name: "[0,1] then result",
+			args: args{[]int{0, 1}},
+			want: [][]int{{0, 1}, {1, 0}},
+		},
+		{
+			name: "[1,2,3] then result",
+			args: args{nums: []int{1, 2, 3}},
+			want: [][]int{{1, 2, 3}, {1, 3, 2}, {2, 1, 3}, {2, 3, 1}, {3, 1, 2}, {3, 2, 1}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := permute(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("permute() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
