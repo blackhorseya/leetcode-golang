0cd2a16d0dedea65b06d8733b2b7126a62b0f721
Sean Zheng <blackhorseya@gmail.com>
2021-08-21 22:20:12 +0800

    feat: add problem 0121

---
+++ leetcode/problem-0121/0121.go
@@ -0,0 +1,17 @@
+package problem_0121
+
+func maxProfit(prices []int) int {
+	profit, buyFlag := 0, 0
+
+	for i, price := range prices[1:] {
+		if price < prices[buyFlag] {
+			buyFlag = i + 1
+		}
+
+		if price-prices[buyFlag] > profit {
+			profit = price - prices[buyFlag]
+		}
+	}
+
+	return profit
+}
