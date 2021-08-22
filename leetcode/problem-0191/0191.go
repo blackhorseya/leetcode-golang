f82f4fee76abaec5c5ad933cf059754cf79a263e
Sean Zheng <blackhorseya@gmail.com>
2021-08-22 18:29:26 +0800

    feat: add 0191

---
+++ leetcode/problem-0191/0191.go
@@ -0,0 +1,15 @@
+package problem_0191
+
+import "fmt"
+
+func hammingWeight(num uint32) int {
+	str := fmt.Sprintf("%b", num)
+	count := 0
+	for _, ch := range str {
+		if ch == '1' {
+			count++
+		}
+	}
+
+	return count
+}
---
+++ leetcode/problem-0191/0191_test.go
@@ -0,0 +1,27 @@
+package problem_0191
+
+import "testing"
+
+func Test_hammingWeight(t *testing.T) {
+	type args struct {
+		num uint32
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "11 then 3",
+			args: args{num: 11},
+			want: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := hammingWeight(tt.args.num); got != tt.want {
+				t.Errorf("hammingWeight() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
