627c3efb46c170f868bbbceaf12c7367ca304fb1
Sean Zheng <blackhorseya@gmail.com>
2021-08-22 14:28:25 +0800

    feat: add 0338

---
+++ leetcode/problem-0338/0338.go
@@ -0,0 +1,21 @@
+package problem_0338
+
+import "fmt"
+
+func countBits(n int) []int {
+	ret := make([]int, n+1)
+	for n >= 0 {
+		count := 0
+		for _, val := range fmt.Sprintf("%b", n) {
+			if val == '1' {
+				count++
+			}
+		}
+
+		ret[n] = count
+
+		n--
+	}
+
+	return ret
+}
---
+++ leetcode/problem-0338/0338_test.go
@@ -0,0 +1,35 @@
+package problem_0338
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_countBits(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "2 then [0,1,1]",
+			args: args{n: 2},
+			want: []int{0, 1, 1},
+		},
+		{
+			name: "5 then [0,1,1,2,1,2]",
+			args: args{n: 5},
+			want: []int{0, 1, 1, 2, 1, 2},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countBits(tt.args.n); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("countBits() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
