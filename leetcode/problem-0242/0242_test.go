5ccc0bd431f73903b2a4d1ee3aef021fe1dc4971
Sean Zheng <blackhorseya@gmail.com>
2021-08-22 18:57:30 +0800

    feat: add 0242

---
+++ leetcode/problem-0242/0242_test.go
@@ -0,0 +1,28 @@
+package problem_0242
+
+import "testing"
+
+func Test_isAnagram(t *testing.T) {
+	type args struct {
+		s string
+		t string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "anagram nagaram then true",
+			args: args{s: "anagram", t: "nagaram"},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isAnagram(tt.args.s, tt.args.t); got != tt.want {
+				t.Errorf("isAnagram() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
