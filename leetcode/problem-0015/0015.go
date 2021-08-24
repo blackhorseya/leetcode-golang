3c5ad5a0fbcedb42126bb7d4a70f6d21d237c754
Sean Zheng <blackhorseya@gmail.com>
2021-08-24 22:32:02 +0800

    feat: add 0015

---
+++ leetcode/problem-0015/0015.go
@@ -0,0 +1,36 @@
+package problem
+
+import "sort"
+
+func threeSum(nums []int) [][]int {
+	var ret [][]int
+	sort.Ints(nums)
+	for i := 0; i < len(nums)-2; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+
+		target, left, right := -nums[i], i+1, len(nums)-1
+		for left < right {
+			sum := nums[left] + nums[right]
+			if sum == target {
+				ret = append(ret, []int{nums[i], nums[left], nums[right]})
+				left++
+				right--
+
+				for left < right && nums[left] == nums[left-1] {
+					left++
+				}
+				for left < right && nums[right] == nums[right+1] {
+					right--
+				}
+			} else if sum > target {
+				right--
+			} else if sum < target {
+				left++
+			}
+		}
+	}
+
+	return ret
+}
