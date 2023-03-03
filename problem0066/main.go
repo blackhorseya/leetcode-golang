38937d1757bd34ebc16db22556c33b3eb0a68a56
Sean Zheng <blackhorseya@gmail.com>
2023-03-04 07:31:05 +0800

    feat: impl 0066

---
+++ problem0066/main.go
@@ -0,0 +1,20 @@
+package main
+
+func plusOne(digits []int) []int {
+	n := len(digits)
+
+	for i := n - 1; i >= 0; i-- {
+		digits[i]++
+		if digits[i] < 10 {
+			return digits
+		} else {
+			digits[i] = 0
+		}
+	}
+
+	return append([]int{1}, digits...)
+}
+
+func main() {
+
+}
