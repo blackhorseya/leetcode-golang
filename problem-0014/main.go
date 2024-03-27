ce25dc2b39fc63380f6909d2818d7a4f26428125
Sean Zheng <blackhorseya@gmail.com>
2024-03-27 23:48:31 +0800

    feat: consolidate codebase for problem-0014
    - Add a new `main.go` file for `problem-0014`
    - Add a new `main_test.go` file for `problem-0014`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ problem-0014/main.go
@@ -0,0 +1,19 @@
+package main
+
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+
+	prefix := strs[0]
+	for i := 1; i < len(strs); i++ {
+		for j := 0; j < len(prefix); j++ {
+			if j == len(strs[i]) || prefix[j] != strs[i][j] {
+				prefix = prefix[:j]
+				break
+			}
+		}
+	}
+
+	return prefix
+}
