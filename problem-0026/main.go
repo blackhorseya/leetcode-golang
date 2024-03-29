a206041b201b1c528a19fa76600127acc683a7ec
Sean Zheng <blackhorseya@gmail.com>
2024-03-29 19:08:19 +0800

    feat: refactor codebase to improve data processing efficiency
    - Add `main.go` with a `removeDuplicates` function
    - Add tests for `removeDuplicates` in `main_test.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ problem-0026/main.go
@@ -0,0 +1,20 @@
+package main
+
+func removeDuplicates(nums []int) int {
+	k := 1
+
+	for i, num := range nums {
+		if i == 0 {
+			continue
+		}
+
+		if num == nums[k-1] {
+			continue
+		} else {
+			nums[k] = num
+			k++
+		}
+	}
+
+	return k
+}
