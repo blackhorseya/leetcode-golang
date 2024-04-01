6bae82f878e3a5e7da9eec89536741d201e13189
Sean Zheng <blackhorseya@gmail.com>
2024-04-01 10:14:44 +0800

    feat: improve array duplicate checking functionality
    - Add a new file `problem-0217.go` with a function to check for duplicates in an array of integers
    - Add a new file `problem-0217_test.go` with tests for the `containsDuplicate` function
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ problems/problem-0217.go
@@ -0,0 +1,13 @@
+package problems
+
+func containsDuplicate(nums []int) bool {
+	m := make(map[int]int)
+	for _, num := range nums {
+		if _, ok := m[num]; ok {
+			return true
+		}
+		m[num] = 1
+	}
+
+	return false
+}
