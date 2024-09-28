e8da792cc203985c1133c4677d4e4a170e206db0
Sean Zheng <blackhorseya@gmail.com>
2024-09-28 13:55:49 +0800

    feat: refactor intersection logic and improve test coverage
    - Modify the `intersect` function to handle cases where `nums1` is shorter than `nums2`
    - Update the `intersect` function to correctly handle situations where an element occurs more than once
    - Add test cases for different scenarios in the `Test_intersect` function
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- problems/problem0350.go
+++ problems/problem0350.go
@@ -1,1 +1,31 @@
 package problems
+
+func intersect(nums1 []int, nums2 []int) []int {
+	if len(nums1) < len(nums2) { // 確保 nums1 是較長的 slice
+		nums1, nums2 = nums2, nums1
+	}
+
+	// 執行交集運算
+	// 建立 nums2 中每個元素的出現次數 [num]count
+	m := make(map[int]int)
+	for _, num := range nums2 {
+		m[num]++
+	}
+
+	res := []int{}
+	for _, num := range nums1 {
+		count, ok := m[num]
+		if !ok {
+			continue
+		} else {
+			res = append(res, num)
+			if count > 1 {
+				m[num]--
+			} else {
+				delete(m, num)
+			}
+		}
+	}
+
+	return res
+}
--- problems/problem0350_test.go
+++ problems/problem0350_test.go
@@ -1,1 +1,98 @@
 package problems
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_intersect(t *testing.T) {
+	type args struct {
+		nums1 []int
+		nums2 []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "example 1",
+			args: args{
+				nums1: []int{1, 2, 2, 1},
+				nums2: []int{2, 2},
+			},
+			want: []int{2, 2},
+		},
+		{
+			name: "example 2",
+			args: args{
+				nums1: []int{4, 9, 5},
+				nums2: []int{9, 4, 9, 8, 4},
+			},
+			want: []int{9, 4},
+		},
+		{
+			name: "no intersection",
+			args: args{
+				nums1: []int{1, 2, 3},
+				nums2: []int{4, 5, 6},
+			},
+			want: []int{},
+		},
+		{
+			name: "empty nums1",
+			args: args{
+				nums1: []int{},
+				nums2: []int{1, 2, 3},
+			},
+			want: []int{},
+		},
+		{
+			name: "empty nums2",
+			args: args{
+				nums1: []int{1, 2, 3},
+				nums2: []int{},
+			},
+			want: []int{},
+		},
+		{
+			name: "both empty",
+			args: args{
+				nums1: []int{},
+				nums2: []int{},
+			},
+			want: []int{},
+		},
+		{
+			name: "one element intersection",
+			args: args{
+				nums1: []int{1, 2, 3},
+				nums2: []int{3, 4, 5},
+			},
+			want: []int{3},
+		},
+		{
+			name: "multiple identical elements",
+			args: args{
+				nums1: []int{1, 1, 1, 2, 2, 3},
+				nums2: []int{1, 1, 2, 3, 3},
+			},
+			want: []int{1, 1, 2, 3},
+		},
+		{
+			name: "nums1 larger than nums2",
+			args: args{
+				nums1: []int{1, 1, 1, 1, 1, 2, 2, 3},
+				nums2: []int{1, 2, 3},
+			},
+			want: []int{1, 2, 3},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := intersect(tt.args.nums1, tt.args.nums2); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("intersect() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
